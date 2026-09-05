109a3b5c54c9abb7a3cefd525f74e1ba819a995d
Iskander Baras <baras.iskander@gmail.com>
2026-09-05 10:13:57 +0300

    l

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,10 @@ import { Stack } from "expo-router";
 import '../../global.css';
 export default function RootLayout() {
     return (
-        <Stack>
+        <Stack screenOptions={{
+            // Hide the header for all other routes.
+            headerShown: false,
+        }}>
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="music" options={{ headerShown: false }} />
         </Stack>
--- app/(tabs)/music.tsx
+++ app/(tabs)/music.tsx
@@ -1,5 +1,5 @@
+import { Link } from "expo-router";
 import { ImageBackground, Text, TouchableHighlight, View } from "react-native";
-
 const backgroundImage = require("../assets/background.gif")
 
 export default function Music() {
@@ -12,7 +12,9 @@ export default function Music() {
             className="h-screen place-items-center place-content-center"
         >
             <View className="w-screen h-screen bg-black/50 place-items-center place-content-center">
-
+                <Link href={"/"} className="absolute top-0  left-0 p-5 border m-4 rounded-fulls h-10 w-10 place-items-center rounded-lg place-content-center  border-red">
+                    <Text className="text-white">{"<"}</Text>
+                </Link>
                 <View>
                     <Text className="text-white text-4xl">
                         Some text
